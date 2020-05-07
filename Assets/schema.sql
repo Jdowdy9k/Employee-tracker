769a45c69a54263fc4c0807057a9d76bf1a43a37
Jdowdy <Jdowdy9k@gamil.com>
2020-05-07 17:32:34 -0400

    worked on seed data

--- Assets/schema.sql
+++ Assets/schema.sql
@@ -4,21 +4,21 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     dep_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE emp_role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
     dep_id INT NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE employees (
-    ind INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
--- Assets/seeds.sql
+++ Assets/seeds.sql
@@ -1,6 +1,20 @@
-INSERT INTO employees (name) VALUES ('Meeses');
-INSERT INTO employees (name) VALUES ('Bobbi');
-INSERT INTO employees (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO employees (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO employees (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO employees (name) VALUES ('Owens');
+INSERT INTO department (dep_name) VALUES ('Carpentry');
+INSERT INTO emp_role (title, salary, dep_id) VALUES ('Master Carpenter', 45000, 11);
+INSERT INTO emp_role (title, salary, dep_id) VALUES ('Carpenter', 30000, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Bob','smalls',1,11);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ('Mike','Roswell',1);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ('Jessica','Knight',1);
+
+INSERT INTO department (dep_name) VALUES ('Electrics');
+INSERT INTO emp_role (title, salary, dep_id) VALUES ('Master Electrician', 45000, 22);
+INSERT INTO emp_role (title, salary, dep_id) VALUES ('Electrician', 30000, 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('BJ','Wilks',2,22);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ('Dana','Stern',2);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ('Joshua','Knight',2);
+
+INSERT INTO department (dep_name) VALUES ('Props');
+INSERT INTO emp_role (title, salary, dep_id) VALUES ('Props Master', 45000, 33);
+INSERT INTO emp_role (title, salary, dep_id) VALUES ('Prop Artisan', 30000, 3);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Coraline','Westin',3,33);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ('Kelly','Rondel',3);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ('Garth','plagious II',3);
